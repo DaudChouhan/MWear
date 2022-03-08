3eb2821cfbfe6ecaffe5b687aaaa543737d398c8
kunwar dawood chouhan <kunwar_dawood@live.com>
2022-03-08 19:43:52 +0500

    single product page start

--- MWear/Controllers/ProductDetailController.cs
+++ MWear/Controllers/ProductDetailController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MWear.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,33 @@ namespace MWear.Controllers
 {
     public class ProductDetailController : Controller
     {
+
+
+        mwearEntities db = new mwearEntities();
         // GET: ProductDetail
-        public ActionResult Index()
+        public ActionResult Index(string ProID)
         {
-            return View();
+
+            ViewBag.Products = db.Products.Where(x => x.Active == true).ToList();
+            var categories = db.Categories.Where(x => x.Active == true).ToList();
+            ViewBag.Categories = categories;
+            var productCategory = db.ProductCategories.ToList();
+            ViewBag.productCategory = productCategory;
+
+            var colors = db.Colors.Where(x => x.Active == true).ToList();
+            ViewBag.Colors = colors;
+            var productColor = db.AvailabeColors.ToList();
+            ViewBag.productColor = productColor;
+
+
+            var sizes = db.Sizes.Where(x => x.Active == true).ToList();
+            ViewBag.Sizes = sizes;
+            var productSize = db.AvailableSizes.ToList();
+            ViewBag.productSize = productSize;
+
+
+            var prod = db.Products.Where(x => x.ProductGUID == ProID).FirstOrDefault();
+            return View(prod);
         }
     }
 }
